a2ffb54985a633ac6a2dd141fcb293ba157a6ecd
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-06-08 17:18:04 +0000

    Init commit

---
+++ src/main/kotlin/com/examengine/Exam_engine/ExamEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.examengine.Exam_engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExamEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExamEngineApplication>(*args)
+}
---
+++ src/test/kotlin/com/examengine/Exam_engine/ExamEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.examengine.Exam_engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExamEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
