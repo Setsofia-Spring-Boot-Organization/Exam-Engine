304f3d553f5022de169aee43e4b7cb7f7733b78d
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-06-10 14:04:27 +0000

    Code update: The users can sign up and log into the app

--- src/main/kotlin/com/examengine/exam_engine/exceptions/MyExceptionPayload.kt
+++ src/main/kotlin/com/examengine/exam_engine/exceptions/MyExceptionPayload.kt
@@ -1,6 +1,14 @@
 package com.examengine.exam_engine.exceptions
 
-data class MyExceptionPayload(
+class MyExceptionPayload(
     private val status: Int,
     private val message: String
-)
+) {
+    fun getMessage() : String {
+        return this.message
+    }
+
+    fun getStatus() : Int {
+        return this.status
+    }
+}
