36d0b76b39f9a581fd48e2e4157d1209acb593a9
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-06-11 10:26:23 +0000

    Added the DockerFile... Dockerfile setup.

--- src/main/kotlin/com/examengine/exam_engine/repositories/UserRepository.kt
+++ src/main/kotlin/com/examengine/exam_engine/repositories/UserRepository.kt
@@ -1,11 +1,11 @@
 package com.examengine.exam_engine.repositories
 
 import com.examengine.exam_engine.entities.Users
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.Optional
 
 @Repository
-interface UserRepository : JpaRepository<Users, String> {
+interface UserRepository : CrudRepository<Users, String> {
     fun findByUserEmail(email: String) : Optional<Users>
 }
