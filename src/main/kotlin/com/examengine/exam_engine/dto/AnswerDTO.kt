16cf531b5575ca84bad3b555cf57c7c85ee86a9d
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-06-26 20:26:50 +0000

    Update: Teachers can create questions and students can view questions created by their teachers. Students can also answer questions and see their results.

--- src/main/kotlin/com/examengine/exam_engine/dto/AnswerDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.examengine.exam_engine.dto
-
-data class AnswerDTO(
-    var correct: List<String>,
-    var wrong: List<String>
-)
--- src/main/kotlin/com/examengine/exam_engine/dto/StudentAnswers.kt
+++
@@ -1,6 +0,0 @@
-package com.examengine.exam_engine.dto
-
-data class StudentAnswers(
-    var id: Int,
-    var answer: String,
-)
